1a41faad8be16e69a49696299cd94f1bf94de13d
Bárbara C. Blank Garibaldi <barbaracbgaribaldi@gmail.com>
2020-03-29 16:03:11 -0300

    Updates

---
+++ precache-manifest.1fce94298330b615b1070e84a5f9e90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75aabe0d431c9446057004e13b5f3b38",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6ac5e4c7b20715e9fe90",
+    "url": "/monsters-rolodex/static/css/main.715b6e0c.chunk.css"
+  },
+  {
+    "revision": "4ecb9f0df0474c1b1fa9",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac5e4c7b20715e9fe90",
+    "url": "/monsters-rolodex/static/js/main.6097c6c2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
